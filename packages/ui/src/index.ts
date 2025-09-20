85347ca8406478434b3f878e90e176eaf8f0b600
DrDrewCain <mark@sturman.org>
2025-09-19 23:01:52 -0500

    fix to use the current import format

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,5 @@
 // Export all UI components
-export { Button } from './components/button'
-export { Card } from './components/card'
-export { Input } from './components/input'
-export { Label } from './components/label'
+export { Button } from './components/Button'
+export { Card } from './components/Card'
+export { Input } from './components/Input'
+export { Label } from './components/Label'
